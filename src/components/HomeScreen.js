6130019a120b5cc27fd2680797592725484f33a3
Federico Giovannini <giovannini.federico@hotmail.it>
2021-03-31 20:41:13 +0200

    Navbar completed

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./HomeScreen.css";
 import Nav from "./Nav";
+import Banner from "./Banner";
 
 function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav />
 
-      {/* banner */}
+      <Banner />
       {/* Row */}
     </div>
   );
 }
 
 export default HomeScreen;
+
+//1:09:00
