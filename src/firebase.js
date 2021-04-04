3702d0ff1d5f2ab4e195f715856a9d5ec443275f
Federico Giovannini <giovannini.federico@hotmail.it>
2021-04-04 13:44:55 +0200

    Sign in page

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBvCfzppmHILeYaigM9bA3oZjCKLDrZeNU",
   authDomain: "netflix-clone-4468e.firebaseapp.com",
---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./SignInScreen.css";
+
+function SignInScreen() {
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="signInScreen">
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder="Email" />
+        <input placeholder="Password" type="password" />
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
+        <h4>
+          <span className="signInScreen__gray">New to Netflix? </span>
+          <span className="signInScreen__link" onClick={register}>
+            Sign Up now.
+          </span>
+        </h4>
+      </form>
+    </div>
+  );
+}
+
+export default SignInScreen;
